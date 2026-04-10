293c802fdb350083c2eb7b4f3468ac8f471e083d
ToroDev-Ux <odetorodanieloluwatofunmi@gmail.com>
2026-04-10 09:23:53 -0700

    media updated

--- index.js
+++ index.js
@@ -60,3 +60,9 @@ async function generateUser() {
     </div>
     `;
 }
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
